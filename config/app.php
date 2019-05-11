29c605d50c5d0c3421045e0c06977d9aa921368a
Ricardo Bomfim <ricardoub@gmail.com>
2019-05-11 11:56:39 -0300

    Revert "ep.18 - DevMarketer/EasyNav install"
    This reverts commit 48d506920e9f9f1ea55e2ec1d18a6a6c3f518b51.

--- config/app.php
+++ config/app.php
@@ -226,8 +226,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'Nav' => DevMarketer\EasyNav\EasyNavFacade::class,
-
     ],
 
 ];
