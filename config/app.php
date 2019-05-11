27b5dd810fa6cd08b679c25c1de8e9429e31380a
Ricardo Bomfim <ricardoub@gmail.com>
2019-05-11 12:51:27 -0300

    ep.18 - Devmarketer Easynav install

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        DevMarketer\EasyNav\EasyNavServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Nav' => DevMarketer\EasyNav\EasyNavFacade::class,
 
     ],
 
