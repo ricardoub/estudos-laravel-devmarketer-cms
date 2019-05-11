163f81e509fb9b50fa7de3aa3e71e8f024c34781
Ricardo Bomfim <ricardoub@gmail.com>
2019-05-11 13:25:14 -0300

    ep.18 highlight active navigation elements

--- public/js/app.js
+++ public/js/app.js
@@ -56262,6 +56262,13 @@ if (token) {
 var accordions = document.getElementsByClassName('has-submenu');
 
 for (var i = 0; i < accordions.length; i++) {
+  if (accordions[i].classList.contains('is-active')) {
+    var submenu = accordions[i].nextElementSibling;
+    submenu.style.maxHeight = submenu.scrollHeight + "px";
+    submenu.style.marginTop = "0.75em";
+    submenu.style.marginBottom = "0.75em";
+  }
+
   accordions[i].onclick = function () {
     this.classList.toggle('is-active');
     var submenu = this.nextElementSibling;
